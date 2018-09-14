d0b6286342b71ac5ebb5005c4965494ee778ba65
wangfei52 <Plpl.1007!>
2018-09-14 16:09:19 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template >
+
+  <div>
+    <el-button type="primary" @click="clickFn">跳转</el-button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "wang",
+      lastName: "fei",
+      count: 0,
+      show: true
+    };
+  },
+  computed: {},
+  //监听
+  watch: {},
+  methods: {
+    clickFn: function() {
+      this.$router.push({ path: "/loginCenter" });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/page/loginCenter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <el-row type="flex" justify="center" >
+      <el-col :span=5 >
+        <el-card class="login-card">
+          <div slot="header" class="clearfix">
+            <span class="sso-title">SSO统一登录平台</span>
+          </div>
+          <el-form>
+            <el-form-item>
+              <el-input v-model="account" placeholder="请输入邮箱">
+              </el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-input v-model="password" type="password" placeholder="请输入密码">
+                </el-input>
+            </el-form-item>
+            <el-form-item>
+               <el-button type="primary">登录</el-button>
+               <el-button class="register-button">注册</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    account: "";
+    password: "";
+  }
+};
+</script>
+
+<style scoped>
+.login-card {
+  width: 400px;
+  height: 300px;
+  margin-top: 50px;
+}
+.sso-title{
+  font-family: Helvetica;
+  font-weight:bold;
+}
+.register-button{
+  margin-left: 80px;
+}
+</style>
+
